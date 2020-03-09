e9c6e6d3a66d5dbdb25cbefab265e1f33b33ac4c
coryortega <coryortega3@gmail.com>
2020-03-09 11:25:22 -0700

    completed the first 5

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,19 +8,19 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+print(x.append(4))
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+print(x.extend(y))
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+print(x.remove(8))
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+print(x.insert(1, 1))
 
 # Print the length of list x
 # YOUR CODE HERE
